2dc46812a693c5d66fa31c4888277ca56c21112f
zhanghe <15942892352@sina.cn>
2019-11-05 09:32:33 +0800

    feat:头部右侧,用户信息根据是否记住密码进行相应存储,登录功能的实现

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,5 +1,7 @@
 import {combineReducers} from 'redux'
 import menus from './menus'
+import user from './user'
 export default combineReducers({
   menus,
+  user
 })
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,9 +1,81 @@
 import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import {Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {startLogin} from '../../actions/menuAction'
+import './login.less'
+
+const mapState = state => ({
+  isLogin:state.user.isLogin
+})
+@connect(mapState,{startLogin})
+@Form.create()
 class Login extends Component {
+  constructor(){
+    super()
+    this.state = {
+      rememberStatus:true
+    }
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.props.startLogin(values)
+      }
+    });
+    // CI/CD
+  };
+  onChange = e => {
+    this.setState({
+      rememberStatus:e.target.checked
+    })
+  }
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        Login
+      this.props.isLogin ?
+      <Redirect to="/admin" />
+      :
+      <div className="login-wrap">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入用户名' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="请输入用户名"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [
+                { required: true, message: '请输入密码' },
+                { min:6,max:12, message: '长度为6-12' }
+              ],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="请输入密码"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: this.state.rememberStatus,
+            })(<Checkbox
+              onChange={this.onChange}
+            >记住密码</Checkbox>)}
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录  
+            </Button>
+            <Button type="primary">注册</Button>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
